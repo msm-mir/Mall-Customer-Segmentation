c235c00e4e598fae423b8d38693c45c935109426
marble <msm.mirdamad@gmail.com>
2026-05-23 23:46:53 +0330

    add pairplot segmented by Gender

--- src/src.py
+++ src/src.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # read dataset
 df = pd.read_csv('Mall_Customers.csv')
+
+# drop CustomerID column
+df = df.drop('CustomerID', axis=1)
+
+# pairplot including gender
+sns.pairplot(df, hue='Gender')
+plt.suptitle("Pairplot segmented by Gender", y=1.02)
+plt.show()
